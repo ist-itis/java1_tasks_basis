d640eda002ea23c0fabcdd38710f4243964db532
Alexander Ferenets <ist.kazan@gmail.com>
2020-10-26 21:35:01 +0300

    Test example + better test output

---
+++ src/test/java/ru/kpfu/itis/AppTest.java
@@ -0,0 +1,11 @@
+package ru.kpfu.itis;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test()
+    public void alwaysPass(){
+        Assertions.assertTrue(false, "Always passing test.");
+    }
+}
