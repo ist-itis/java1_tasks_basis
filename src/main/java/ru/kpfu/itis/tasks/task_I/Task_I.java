5a587ca89028300b290b3b7fdc1df6bc0e112bc5
Alexander Ferenets <ist.kazan@gmail.com>
2020-10-30 21:28:33 +0300

    Task I

---
+++ src/main/java/ru/kpfu/itis/tasks/task_I/Task_I.java
@@ -0,0 +1,8 @@
+package ru.kpfu.itis.tasks.task_I;
+
+/**
+ * Task I. Create 3 classes with inheritance in this package.
+ * At least one of classes must contain at least 5 fields with getters-setters.
+ */
+public class Task_I {
+}
---
+++ src/test/java/ru/kpfu/itis/tasks/task_I/Task_ITest.java
@@ -0,0 +1,4 @@
+package ru.kpfu.itis.tasks.task_I;
+
+public class Task_ITest {
+}
