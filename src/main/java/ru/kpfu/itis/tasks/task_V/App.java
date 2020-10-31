5bb939f11a8ddd6e0327a4ec619405d42a842f41
Alexander Ferenets <ist.kazan@gmail.com>
2020-10-31 14:00:29 +0300

    Task V

---
+++ src/main/java/ru/kpfu/itis/tasks/task_V/App.java
@@ -0,0 +1,7 @@
+package ru.kpfu.itis.tasks.task_V;
+
+public class App {
+    public static void main(String[] args) {
+        // Some code here
+    }
+}
---
+++ src/test/java/ru/kpfu/itis/tasks/task_V/AppTest.java
@@ -0,0 +1,4 @@
+package ru.kpfu.itis.tasks.task_V;
+
+public class AppTest {
+}
